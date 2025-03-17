ff7f568ec755ae5f5dd1511c9fdb5907df075e81
Jordi <jordisegura2002@gmail.com>
2025-03-17 19:56:28 +0100

    fixed not receving own messages

--- src/can/mod.rs
+++ src/can/mod.rs
@@ -8,29 +8,28 @@ use socketcan::{tokio::CanSocket, CanFrame, CanId, EmbeddedFrame, Frame, SocketO
 use tokio::sync::mpsc::Receiver;
 
 pub async fn can_perioic_task(mut rx: Receiver<CanMessage>, interface: String) {
-    let socket_rx = CanSocket::open(&interface);
-    let socket_tx = CanSocket::open(&interface);
+    let socket = CanSocket::open(&interface);
 
-    let (mut socket_rx, socket_tx) = if socket_rx.is_err() || socket_tx.is_err() {
+    let mut socket  = if socket.is_err() {
         eprintln!("Couldn't open socketcan interface: {}", interface);
         std::process::exit(-1);
     } else {
-        (socket_rx.unwrap(), socket_tx.unwrap())
+        socket.unwrap()
     };
 
-    let loop_back_result = socket_tx.set_loopback(false);
+    let recv_own_messages_result = socket.set_recv_own_msgs(false);
 
-    if loop_back_result.is_err()  {
-        eprint!("Couldn't open socketcan interface: {}", interface);
+    if recv_own_messages_result.is_err()  {
+        eprint!("Couldn't configure socketcan interface: {}", interface);
         std::process::exit(-1);
     }
 
     loop {
         tokio::select! {
             Some(message) = rx.recv() => {
-                socket_tx.write_frame(CanFrame::new(CanId::standard(message.can_id as u16).unwrap(), &message.body).unwrap()).await.unwrap();
+                socket.write_frame(CanFrame::new(CanId::standard(message.can_id as u16).unwrap(), &message.body).unwrap()).await.unwrap();
             }
-            Some(container) = socket_rx.next() => {
+            Some(container) = socket.next() => {
                 if let Ok(can_frame) = container {
                     enqueue(CanMessage::new(can_frame.can_id().as_raw() as u16, can_frame.data())).await;
                 }
