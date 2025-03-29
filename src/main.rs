61b86dffdf2b357d85e0a8709621bf40cfc1fa73
Jordi <jordisegura2002@gmail.com>
2025-03-29 09:46:29 +0100

    made api port a parameter

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ mod can;
 struct Args {
     //Socketcan Interface's name
     interface: String,
+    //TCP port used by the API
+    port: u32,
 }
 
 #[tokio::main]
@@ -19,6 +21,6 @@ async fn main() {
     tokio::spawn(can::can_perioic_task(rx, args.interface));
 
     warp::serve(api::api_filter(tx))
-        .run(([127, 0, 0, 1], 3030))
+        .run(([0, 0, 0, 0], 3030))
         .await;
 }
